438061d9f2903d506047af224066c38234169321
chikoyoshi <chikoyoshi-21141@outlook.jp>
2016-10-13 01:03:18 +0900

    完成(コメント追加)

--- pro4_1-2.c
+++ pro4_1-2.c
@@ -7,7 +7,7 @@
 #include "stack.h"
 int main()
 {
-  int ch, pch, small;
+  int ch, pch, small, i;
   small = 0;//小文字で始まる単語の時は1にする
   init_stack(); //スタックの初期化
   fprintf(stdout,"Please enter English text: \n");
@@ -18,23 +18,36 @@ int main()
     if(ch != ' ')
     {
       //スペースでないときの処理を書く
-      if(pch == ' ' && ch >= 0x61 && ch <= 0x7a)
-        {
-          push(ch);
-          small = 1;
-        }
-      else if(small == 1)
-        {
-          push(ch);
-        }
+      if(pch == ' ' && ch >= 0x61 && ch <= 0x7a)  //前の文字がスペースでその文字が小文字の時
+      {
+        push(ch);                                 //その文字をスタックに格納
+        small = 1;
+      }
+      else if(small == 1)                         //その文字が小文字から始まる英単語ならその文字をスタックに格納
+      {
+        push(ch);
+      }
+      else                                        //それ以外ならそのまま出力
+      fprintf(stdout,"%c",ch);
     }
     else
     {
+      if(small == 0)                              //スペースを押された時点で前の単語が大文字から始まる単語ならスペースを出力
+        fprintf(stdout,"%c",ch);
       //スペースの時の処理を書く
+      if(small == 1)                              //スペースを押された時点で前の単語が小文字から始まる単語ならスタックに保存してあるものを吐き出す
+      {
+        small = 0;
+        while(stack_stock()>0)
+        {
+          printf("%c",pop());
+        }
+          fprintf(stdout," ");
+      }
     }
     pch = ch;
   }
-  while(stack_stock()>0)
+  while(stack_stock()>0)                            //enterが押された時点でスタックにあるものを全て吐き出す.
   {
     fprintf(stdout,"%c",pop());
   }
